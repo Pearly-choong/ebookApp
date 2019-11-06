d5b4ea082f7a0fecb0b442a7a988b9c11ebdacd9
[Pearly-choong] <[choongpl@gmail.com]>
2019-11-06 20:54:11 +1300

    first commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+console.log('BookCentral App');
+
+var books = [
+  {
+    title : 'THE GIRL OF INK & STAR',
+    id  : 'B101',
+    Author  : 'Kiran Millwood Hargrave',
+    Rating : '<i class="fas fa-star"></i>',
+    Price : 18.30,
+    Genre : 'Fantasy',
+    Language  : 'English',
+    Pages   : 500,
+    Published : '05-05-2016',
+    Summary : 'Forbidden to leave her island, Isabella Riosse dreams of the faraway lands her father once mapped.
+    </br> When her closest friend disappears into the island’s Forgotten Territories, she volunteers to guide the search. As a cartographer’s daughter, she’s equipped with elaborate ink maps and knowledge of the stars, and is eager to navigate the island’s forgotten heart'
+  },
+
+  {
+    title : 'THE LABYRINTH',
+    id  : 'B102',
+    Author  : 'Filey Keating',
+    Rating : '<i class="fas fa-star"></i>',
+    Price : 18.30,
+    Genre : 'Romance',
+    Language  : 'English',
+    Pages   : 650,
+    Published : '01-01-2006',
+    Summary : 'What happens when you wish for something terrible ... and your wish comes true? Young Sarah is about to find out. Left at home to mind her baby brother, Toby, she finds herself trying to comfort a screaming infant as a wild storm rages about the house. In a fit of temper, she wishes that the goblins would come and take the child away. Unfortunately, they do.'
+  },
+
+  {
+    title : 'PERFUME',
+    id  : 'B103',
+    Author  : 'Lizzie Ostrom',
+    Rating : '<i class="fas fa-star"></i>',
+    Price : 15.00,
+    Genre : 'Romance',
+    Language  : 'English',
+    Pages   : 384,
+    Published : '22-10-2015',
+    Summary : 'With brilliantly exuberant style, perfume expert Lizzie Ostrom dives into a world of fragrance, introducing us to signature scents; now-lost masterpieces; and potent perfumes from across the decades. She reveals the wild and wonderful launch campaigns; the visionaries behind these mysterious concoctions; and the loyal fans of these little bottles of delight, who sprayed them in abundance.'
+  },
+
+  {
+    title : 'ROSEMARY\'S BABYE',
+    id  : 'B104',
+    Author  : 'Ira Levin',
+    Rating : '<i class="fas fa-star"></i>',
+    Price : 15.50,
+    Genre : 'Romance',
+    Language  : 'English',
+    Pages   : 308,
+    Published : '01-09-1997',
+    Summary : 'Rosemary and Guy Woodhouse, an ordinary young couple, settle into a New York City apartment, unaware that the elderly neighbors and their bizarre group of friends have taken a disturbing interest in them. But by the time Rosemary discovers the horrifying truth, it may be far too late!'
+  },
+
+  {
+    title : 'A SPELL FOR CHAMELEON',
+    id  : 'B105',
+    Author  : 'Piers Anthony',
+    Rating : '<i class="fas fa-star"></i>',
+    Price : 20.50,
+    Genre : 'Fantasy',
+    Language  : 'English',
+    Pages   : 308,
+    Published : '01-09-1997',
+    Summary : 'Rosemary and Guy Woodhouse, an ordinary young couple, settle into a New York City apartment, unaware that the elderly neighbors and their bizarre group of friends have taken a disturbing interest in them. But by the time Rosemary discovers the horrifying truth, it may be far too late!'
+  },
+
+];
